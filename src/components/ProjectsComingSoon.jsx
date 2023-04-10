4d1f8bb40acc58722068233f02e9bdf1b536c013
Ross Gordon <rossagordonstl@gmail.com>
2023-04-10 10:43:00 -0400

    Projects coming soon updated

--- src/components/ProjectsComingSoon.jsx
+++ src/components/ProjectsComingSoon.jsx
@@ -10,8 +10,8 @@ const Projects = () => {
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full">
         <div className="pb-8">
           <p className="text-5xl font-bold inline">Projects (Coming Soon)</p>
-          <p className="py-6 text-2xl text-center">
-            I'm excited to share my new projects. Check back soon! <br />
+          <p className="py-6 text-2xl">
+            I'm currently working on new projects. Check back soon! <br />
             For now, check out this{' '}
             <a
               href="https://ai-img-generator.netlify.app/"
