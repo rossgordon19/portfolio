fa4c5707ac3f0fdc48bac6c1e94850314f57d2b2
Ross Gordon <rossagordonstl@gmail.com>
2023-04-15 22:25:14 -0400

    projects coming soon updated

--- src/components/ProjectsComingSoon.jsx
+++ src/components/ProjectsComingSoon.jsx
@@ -9,10 +9,19 @@ const Projects = () => {
       {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full">
         <div className="pb-8">
-          <p className="font-bold text-4xl md:text-5xl my-4">Projects (Coming Soon)</p>
+          <p className="font-bold text-4xl md:text-5xl my-4">
+            Projects (Coming Soon)
+          </p>
           <p className="py-6 text-lg md:text-xl">
-            I'm currently working on new projects. Check back soon! <br />
-            For now, check out this{' '}
+            I'm currently working on new projects. For now, check out my{' '}
+            <a
+              href="https://www.github.com/rossgordon19"
+              target="_blank"
+              className="underline cursor-pointer"
+            >
+              GitHub
+            </a>
+            , or this <br />{' '}
             <a
               href="https://ai-img-generator.netlify.app/"
               target="_blank"
